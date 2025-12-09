c56218a11e4f1572cd42390c28cdfc4044cc2d32
M0-o <berrami.mouad2@gmail.com>
2025-12-09 12:02:15 +0100

    initial commit

---
+++ src/main/java/com/yourcompany/expensetracker/MainApp.java
@@ -0,0 +1,23 @@
+package com.yourcompany.expensetracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("Expense Tracker — Prototype");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
