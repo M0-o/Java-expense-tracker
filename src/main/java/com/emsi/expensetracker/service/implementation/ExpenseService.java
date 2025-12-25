53bcc7bbf92e1ee08665508fef454e053a66815f
M0-o <berrami.mouad2@gmail.com>
2025-12-25 22:48:19 +0100

    started implementing expense service

--- src/main/java/com/emsi/expensetracker/service/implementation/ExpenseService.java
+++ src/main/java/com/emsi/expensetracker/service/implementation/ExpenseService.java
@@ -1,5 +1,9 @@
 package com.emsi.expensetracker.service.implementation;
 
+import com.emsi.expensetracker.dao.implementation.ExpenseDAO;
+import com.emsi.expensetracker.model.Expense;
+import com.emsi.expensetracker.service.base.BaseService;
+
 /**
  * Service class for expense management operations. Provides business logic for
  * creating, retrieving, updating, and deleting user expenses. Acts as an
@@ -7,6 +11,21 @@ package com.emsi.expensetracker.service.implementation;
  *
  * TODO: Implement expense CRUD operations and business logic.
  */
-public class ExpenseService {
+public class ExpenseService extends BaseService<ExpenseDAO> {
+
+    public ExpenseService(ExpenseDAO dao) {
+        super(dao);
+    }
+
+    /**
+     * Create a new expense for a user
+     *
+     * @param expense Expense object containing details
+     * @return true if created successfully, false otherwise
+     */
+    public boolean createExpense(Expense expense) {
+        
+        return dao.save(expense);
+    }
 
 }
