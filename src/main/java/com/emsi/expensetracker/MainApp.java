fd1a4a510939f2da1e77a1369bcf4570f7bad6ae
M0-o <berrami.mouad2@gmail.com>
2025-12-25 12:06:42 +0100

    refactored mainApp to initialize dependencies , and inject them into where theyre needed at startup

--- src/main/java/com/emsi/expensetracker/MainApp.java
+++ src/main/java/com/emsi/expensetracker/MainApp.java
@@ -1,44 +1,56 @@
 
 package com.emsi.expensetracker;
 
+import com.emsi.expensetracker.controller.LoginController;
 import com.emsi.expensetracker.util.DatabaseConnection;
+import com.emsi.expensetracker.dao.implementation.AuthDAO;
+import com.emsi.expensetracker.service.implementation.AuthService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class MainApp extends Application {
 
+    private final DatabaseConnection dbConnection = new DatabaseConnection();
+    private final AuthService authService = new AuthService(new AuthDAO(dbConnection));
+
     @Override
     public void init() {
-        DatabaseConnection.initialize();
+        dbConnection.initialize();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
-        primaryStage.setTitle("Expense Tracker");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        showLoginView(primaryStage);
+    }
+
+    public void showLoginView(Stage primaryStage){
+        LoginController controller = new LoginController(this ,authService);
+        Scene scene = loadScene("/fxml/LoginView.fxml", controller);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    // replace after finishing fxml files for login and register
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginView.fxml"));
-//        primaryStage.setTitle("Expense Tracker - Login");
-//        primaryStage.setScene(new Scene(root, 400, 300));
-//        primaryStage.show();
-//    }
-
-
     @Override
     public void stop() {
-        DatabaseConnection.shutdown();
+        dbConnection.shutdown();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public Scene loadScene(String fxmlFile, Object controller) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            loader.setController(controller);
+            Parent root = loader.load();
+            return new Scene(root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
