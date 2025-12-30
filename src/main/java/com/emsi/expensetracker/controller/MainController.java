b08e4116eb6545a19a3ffe43c9cb88658df85299
M0-o <berrami.mouad2@gmail.com>
2025-12-30 09:30:11 +0100

    updated main controller to handle navigation

--- src/main/java/com/emsi/expensetracker/controller/MainController.java
+++ src/main/java/com/emsi/expensetracker/controller/MainController.java
@@ -1,25 +1,29 @@
 package com.emsi.expensetracker.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.emsi.expensetracker.MainApp;
 import com.emsi.expensetracker.service.implementation.AuthService;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import java.net.URL;
-import java.util.ResourceBundle;
-import com.emsi.expensetracker.MainApp;
 
 public class MainController implements Initializable {
-    AuthService authService ;
-    MainApp app ;
 
-    public MainController(MainApp app ,AuthService authService) {
+    AuthService authService;
+    MainApp app;
+
+    public MainController(MainApp app, AuthService authService) {
         this.authService = authService;
         this.app = app;
     }
 
-    @FXML private Label userLabel;
+    @FXML
+    private Label userLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,12 +33,27 @@ public class MainController implements Initializable {
     }
 
     @FXML
+    private void handleViewExpenses() {
+        ExpenseListActionController controller = app.createExpenseListController();
+        Scene scene = app.loadScene("/fxml/ExpenseListView.fxml", controller);
+        Stage stage = (Stage) userLabel.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void handleAddExpense() {
+        SaveExpenseController controller = app.createSaveExpenseController();
+        Scene scene = app.loadScene("/fxml/ExpenseFormView.fxml", controller);
+        Stage stage = (Stage) userLabel.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
+    @FXML
     private void handleLogout() {
         LoginController controller = new LoginController(app, authService);
         Scene scene = app.loadScene("/fxml/LoginView.fxml", controller);
         Stage stage = (Stage) userLabel.getScene().getWindow();
         stage.setScene(scene);
         authService.logout();
-
     }
 }
